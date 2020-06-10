72ed91fc84a7ca64ac715438d5953a537ab50e48
dejiayodeji <deji@tulabyte.net>
2020-06-11 00:21:06 +0100

    countdown
    counts down to  an event

---
+++ main.js
@@ -0,0 +1,35 @@
+var targetDate = moment("08/08/2020");
+
+var $days = $('#days'),
+    $hours = $('#hours'),
+    $minutes = $('#minutes'),
+    $seconds = $('#seconds');
+
+setInterval(function() {
+    //calculate the time remaining
+    var present = moment();
+    timeLeft = moment.duration(targetDate.diff(present));
+    updateClock(timeLeft);
+}, 10);
+
+function updateClock(remainingTime) {
+    // Display time difference
+    var days = Math.floor(remainingTime.asDays()),
+        hours = Math.floor(remainingTime.asHours()) % 24,
+        mins = Math.floor(remainingTime.asMinutes()) % 60,
+        seconds = Math.floor(remainingTime.asSeconds()) % 60;
+
+    $('#days').text(padNumber(days));
+    $('#hours').text(padNumber(hours));
+    $('#minutes').text(padNumber(mins));
+    $('#seconds').text(padNumber(seconds));
+
+}
+
+function padNumber(number) {
+    var formattedNum = number;
+    if (number < 10) {
+        formattedNum = "0" + formattedNum;
+    }
+    return formattedNum;
+}
